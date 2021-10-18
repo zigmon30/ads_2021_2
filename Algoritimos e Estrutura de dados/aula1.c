454d2e7c016fb5a8e451a0c3cdf3219610ad3c54
DOUGLAS KELCZESKI <54138968+zigmon30@users.noreply.github.com>
2021-10-17 23:03:49 -0300

    Initial commit

---
+++ Algoritimos e Estrutura de dados/aula1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct _aluno{
+	char nome[20];
+	int matricula;
+	
+}Aluno;
+
+int main(){
+
+	Aluno a1;
+	strcpy(a1.nome, "João");
+	a1.matricula = 12345;
+
+	printf("endereço de a1=%p\n", &a1);
+	printf("nome de a1=%s\nendereço do nome de a1=%p\n",a1.nome, &a1.nome );
+	printf("matricula de a1=%d\nendereço de matricula de a1=%p\n",a1.matricula, &a1.matricula );
+
+
+
+	/*int i = 5;
+
+	printf("Valor de i=%d \nendereço de i=%p\n",i, &i );
+
+	int v[5];
+	printf("endereço de v=%p\n", &v);
+
+	for(i=0; i<5; i++){
+		v[i] = i;
+		printf("valor de v[%d]=%d \n endereço de v[%d]=%p\n", i, v[i], i, &i);
+	}
+	*/
+
+	return 0;
+}
+
+
+
+
