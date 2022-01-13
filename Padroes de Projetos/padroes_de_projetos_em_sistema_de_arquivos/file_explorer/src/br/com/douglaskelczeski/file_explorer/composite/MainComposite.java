08ac1e51f6f4a25d15952944de386d85d6bc5caf
DOUGLAS KELCZESKI <54138968+zigmon30@users.noreply.github.com>
2022-01-12 22:18:54 -0300

    update final composite

--- Padroes de Projetos/padroes_de_projetos_em_sistema_de_arquivos/file_explorer/src/br/com/douglaskelczeski/file_explorer/composite/MainComposite.java
+++ Padroes de Projetos/padroes_de_projetos_em_sistema_de_arquivos/file_explorer/src/br/com/douglaskelczeski/file_explorer/composite/MainComposite.java
@@ -4,15 +4,37 @@ public class MainComposite {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Audio musica1 = new Audio("Arara", ".mp3", 23);
+		Audio musica1 = new Audio("Arara", ".MP3", 23);
+		Audio musica2 = new Audio("O Segundo Sol", ".MP4", 23);
+		Audio musica3 = new Audio("The Sound of Silence", ".ACC", 23);
+		Audio musica4 = new Audio("Nemesis", ".FLAC", 23);
+		Audio musica5 = new Audio("The World Is Yours", ".MP3", 23);
 		
 		
 		
 		Pasta novaPasta = new Pasta("Misturadas");
 		novaPasta.adicionaMusica(musica1);
+		novaPasta.adicionaMusica(musica2);
+		novaPasta.adicionaMusica(musica3);
+		novaPasta.adicionaMusica(musica4);
+		novaPasta.adicionaMusica(musica5);
+		
+		Pasta novaPasta1 = new Pasta("Rock");
+		novaPasta1.adicionaMusica(musica2);
+		novaPasta1.adicionaMusica(musica3);
+		novaPasta1.adicionaMusica(musica4);
+		novaPasta1.adicionaMusica(musica5);
 		
 		
-		System.out.println("");
+		System.out.println("\nPasta Musicas ==> " + novaPasta.getNomePasta());
+		for(Audio musica : novaPasta.getLAudios()) {
+			System.out.println(musica.getNome() + musica.getFormato());
+		}
+		
+		System.out.println("\nPasta Musicas ==> " + novaPasta1.getNomePasta());
+		for(Audio musica : novaPasta1.getLAudios()) {
+			System.out.println(musica.getNome() + musica.getFormato());
+		}
 
 	}
 
